473b547c24475ff2ccbcdb6594d185e787ac50dc
rupplez <rupplezzz@gmail.com>
2022-10-27 12:03:19 +0900

    new gui system

---
+++ src/gof/gui/GraphicsDriver.java
@@ -0,0 +1,10 @@
+package gof.gui;
+
+import gof.core.Board;
+import gof.core.DisplayDriver;
+
+public class GraphicsDriver implements DisplayDriver {
+	public void displayBoard(Board board) {
+		
+	}
+}
---
+++ src/gof/gui/testClass.java
@@ -0,0 +1,15 @@
+package gof.gui;
+
+import javax.swing.JFrame;
+
+public class testClass {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		
+		f.setTitle("Game of Life");
+		f.setSize(400, 300);
+		f.setVisible(true);
+		
+		
+	}
+}
