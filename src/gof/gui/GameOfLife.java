034cca8de34b5cf2beced3b1eee173d7d5dd88e8
rupplez <rupplezzz@gmail.com>
2022-11-01 08:43:26 +0900

    new graphics system

---
+++ src/gof/gui/GameOfLife.java
@@ -0,0 +1,24 @@
+package gof.gui;
+
+import java.util.Scanner;
+
+import gof.core.Board;
+import gof.core.DisplayDriver;
+
+public class GameOfLife {
+
+    public static void main(String[] args) throws Exception {
+//        System.out.print("Please enter number of iterations to run: ");
+//        Scanner in = new Scanner(System.in);
+//        int iterations = in.nextInt();
+//        in.close();
+        DisplayDriver dd = Display.getDriver();
+        Board b = new Board(30, 30, 0.3);
+
+        while(true) {
+            dd.displayBoard(b);
+            b.update();
+            Thread.sleep(300);
+        }
+    }
+}
--- src/gof/gui/GraphicsDriver.java
+++ src/gof/gui/GraphicsDriver.java
@@ -1,10 +1,52 @@
 package gof.gui;
 
+import java.awt.Graphics;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import gof.core.Board;
+import gof.core.Cell;
 import gof.core.DisplayDriver;
 
-public class GraphicsDriver implements DisplayDriver {
+public class GraphicsDriver extends JFrame implements DisplayDriver {
+	private int x, y;
+	private int size = 10;
+	Color color = Color.black;
+	
+    Cell[][] grid;
+	
 	public void displayBoard(Board board) {
-		
+        setTitle("Game of Life");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        grid = board.getGrid();
+        
+        x = 0;
+        y = 0;
+        
+        setContentPane(new DrawingArea());
+        
+        setSize(300,300);
+        setVisible(true);
 	}
+    class DrawingArea extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(color);
+            for (Cell[] row : grid) {
+                for (Cell c : row) {
+                   if(c.getState()) {
+                       g.fillRect(x,y,size,size);
+                   }
+                   x+=size;
+                   //System.out.println(x+" "+y);
+                }
+                x=0;
+                y+=size;
+            }
+        }
+    }
 }
